f82be13d70daab07d8243158486e632d2fca6c93
keypopsh <vboled@gmail.com>
2021-07-08 14:39:47 +0300

    Forgot commit this file

--- src/main/java/vboled/netcracker/musicstreamer/controllers/ImageController.java
+++ src/main/java/vboled/netcracker/musicstreamer/controllers/ImageController.java
@@ -1,89 +1,57 @@
 package vboled.netcracker.musicstreamer.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import vboled.netcracker.musicstreamer.service.FileControllerService;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.UUID;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/images")
 public class ImageController {
 
-    @Value("${image.storage.path}")
+    private final FileControllerService fileControllerService = new FileControllerService();
+
+    private final Set<String> imageExt = new HashSet<String>(Arrays.asList(".gif", ".png", ".jpeg", ".jpg"));
+
+    // Путь к директории с файлами
+    @Value("${file.storage.path}")
     private String uploadPath;
 
+    // Название субдиректории
+    @Value("${image.storage.dir}")
+    private String imageDir;
+
     @GetMapping("/{uuid}")
+    @PreAuthorize("hasAuthority('admin:perm')")
     public ResponseEntity<?> read(@PathVariable(name = "uuid") String uuid) {
-        try {
-            byte[] image = Files.readAllBytes(Path.of(uploadPath + "/" + uuid));
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(getContentType(uuid));
-            headers.setContentLength(image.length);
-            return new ResponseEntity<>(image, headers, HttpStatus.OK);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return fileControllerService.read(uuid, uploadPath + "/" + imageDir);
     }
 
-    private MediaType getContentType(String uuid) {
-        String ext = uuid.substring(uuid.lastIndexOf('.'));
-        if (ext.equals(".jpeg"))
-            return MediaType.IMAGE_JPEG;
-        else if (ext.equals(".png"))
-            return MediaType.IMAGE_PNG;
-        else if (ext.equals(".gif"))
-            return MediaType.IMAGE_GIF;
-        throw new IllegalArgumentException("Wrong file ext.");
-    }
-
-    @PostMapping("")
-    public ResponseEntity<?> create(@RequestParam("file")MultipartFile file) {
-        if (file != null) {
-            File uploadDir = new File(uploadPath);
-            if (!uploadDir.exists()) {
-                uploadDir.mkdir();
-            }
-            String content = file.getContentType();
-            String ext = "." + content.substring(content.indexOf('/') + 1);
-            String newFileName = UUID.randomUUID().toString() + ext;
-            try {
-                file.transferTo(new File(uploadDir.getAbsolutePath() + "/" + newFileName));
-            } catch (IOException e) {
-                e.printStackTrace();
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-            return new ResponseEntity<>(newFileName, HttpStatus.CREATED);
-        }
-        return new ResponseEntity<>("Wrong file", HttpStatus.BAD_REQUEST);
+    @PostMapping("/")
+    @PreAuthorize("hasAuthority('admin:perm')")
+    public ResponseEntity<?> create(@RequestParam("file") MultipartFile file) {
+        return fileControllerService.uploadFile(file, imageExt, uploadPath + "/" + imageDir);
     }
 
     @DeleteMapping("/{uuid}")
+    @PreAuthorize("hasAuthority('admin:perm')")
     public ResponseEntity<?> delete(@PathVariable(name = "uuid") String uuid) {
-        File file = new File(uploadPath + "/" + uuid);
-        if (file.delete())
-            return  new ResponseEntity<>(HttpStatus.OK);
-        return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+        return fileControllerService.delete(uuid, uploadPath + "/" + imageDir);
     }
 
     @PutMapping("/{uuid}")
+    @PreAuthorize("hasAuthority('admin:perm')")
     public ResponseEntity<?> update(@PathVariable(name = "uuid") String uuid,
-                                    @RequestParam("file")MultipartFile file) {
-        ResponseEntity delResult = delete(uuid);
-        if (delResult.getStatusCode().equals(HttpStatus.OK))
+                                    @RequestParam("file") MultipartFile file) {
+        if (delete(uuid).getStatusCode().equals(HttpStatus.OK))
             return create(file);
-        return delResult;
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
